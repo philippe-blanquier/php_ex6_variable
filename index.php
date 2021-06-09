ce85628ee1101baa54296544d61736911e8953ca
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-09 11:00:12 +0200

    creation

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+        <title>PHP ex6</title>
+    </head>
+    <body >
+        <?php
+        $name="Tweety";
+        echo nl2br("Bonjour ".$name.", comment vas-tu ?\n");
+        ?>
+    </body>
+</html>
